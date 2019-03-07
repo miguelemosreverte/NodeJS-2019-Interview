47176f2d44994176b32ae0a8adb9b9ff1e8ae9a1
miguelemosreverte <miguelemosreverte@gmail.com>
2019-03-07 20:42:45 -0300

    [Refactor][NoExtraFeatures]
     Extracted the win condition from the general rules as it was not 
    actually a validation rule like the rest but a way inferr if a player 
    had won or not.
    
    Conditions are not the same as Validations.
    
     Improved the way a move was being expressed, now it follows the 
    explanation from the documentation as in:
    const move = {
      player: "1",
      coords: {
        x: 0,
        y: 1
      }
    }

--- src/main/TicTacToe/index.js
+++ src/main/TicTacToe/index.js
@@ -1,24 +1,18 @@
 'use strict'
 import {rules}  from './rules'
+import winCondition  from './winConditions'
 import * as R from 'ramda'
 
-const sum = (a, b) => a + b
-
-
-
-const makeMove = (game, move) => ({
-  game,
-  moves: [...game.moves, move]
-})
 
 const validateMove = (rules, gameState) =>
   R.values(rules).map(rule => rule(gameState)).every(e=>e)
 
 
-
+/**/
 const TicTacToe = game => player1 => player2 => rules => gameState => {
   const valid = validateMove(rules, gameState)
-  return valid
+  const won   = winCondition(gameState)
+  return {gameState, valid, won}
 }
 
 export default TicTacToe
--- src/main/TicTacToe/winConditions.js
+++ src/main/TicTacToe/winConditions.js
@@ -3,9 +3,37 @@ import * as R from 'ramda'
 
 
 const winConditions = {
-      groupedByX  : R.groupBy(({move: {coord: {x}}}) => x)
-    , groupedByY  : R.groupBy(({move: {coord: {y}}}) => y)
-    , groupedByXY : R.groupBy(({move: {coord: {x,y}}}) => x-y)
+      groupedByX  : R.groupBy(({coord: {x}}) => x)
+    , groupedByY  : R.groupBy(({coord: {y}}) => y)
+    , groupedByXY : R.groupBy(({coord: {x,y}}) => x-y)
   }
 
-export default winConditions
+
+const win =  ({moves, move}) => {
+
+    const movesByPlayer = R.groupBy(R.prop('player'))(moves)
+    const movesOfThisPlayer = filterWithKeys((player, value) => player == move.player, movesByPlayer)
+
+    const givenPlayerMoves = playerMoves =>
+       R.map( group =>
+           R.keys(group(playerMoves)).length == 1
+           // if all can be grouped in one group then they are the same!
+       )(winConditions)
+
+    return R.values(givenPlayerMoves([move, ...movesOfThisPlayer])).some(a => a)
+}
+
+export default win
+export {winConditions}
+
+
+
+
+
+const filterWithKeys = (pred, obj) => R.pipe(
+  R.toPairs,
+  R.filter(R.apply(pred)),
+  R.fromPairs,
+  R.values,
+  R.flatten
+)(obj);
